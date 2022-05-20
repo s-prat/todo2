c88967214b105eaf5f63927c9a60473b977e98bc
Pratibha <psharma@atlassian.com>
2022-05-20 14:57:45 +0100

    angular project code for To-Do application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { RouteGuardService } from './service/route-guard.service';
+import { TodoUpdateComponent } from './todo-update/todo-update.component';
+import { TodoComponent } from './todo/todo.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component: LoginComponent},
+  {path: 'login' , component: LoginComponent},
+  {path: 'welcome/:name' , component: WelcomeComponent, canActivate: [RouteGuardService]},
+  {path: 'todo' , component: TodoComponent, canActivate: [RouteGuardService]},
+  {path: 'logout' , component: LogoutComponent, canActivate: [RouteGuardService]},
+  {path: 'todo-update/:id' , component: TodoUpdateComponent, canActivate: [RouteGuardService]},
+  {path: '**' , component: ErrorComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ErrorComponent } from './error/error.component';
+import { TodoComponent } from './todo/todo.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogoutComponent } from './logout/logout.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TodoUpdateComponent } from './todo-update/todo-update.component';
+import { HttpInterceptorService } from './service/http/http-interceptor.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    WelcomeComponent,
+    ErrorComponent,
+    TodoComponent,
+    MenuComponent,
+    FooterComponent,
+    LogoutComponent,
+    TodoUpdateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorService, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
